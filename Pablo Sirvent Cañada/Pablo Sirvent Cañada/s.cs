909aed0af9c5744e6805854da8a54527af43db1f
PabloSirCanyada <psirvent78@gmail.com>
2022-05-25 19:34:51 +0200

    Creacion de la rama
    Creamos el archivo visual y la rama

---
+++ Pablo Sirvent Cañada/Pablo Sirvent Cañada/s.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pablo_Sirvent_Cañada
+{
+    public class s
+    {
+    }
+    /*Clase que representa la gestión de los depósitos de una granja.
+* Tiene dos depósitos: pienso y agua. 
+* La propiedad devuelve el nivel de agua y pienso. 
+* Los métodos permiten reponer o consumir tanto pienso como agua.
+* Estos comentarios se deben eliminar y usar comentarios de documentación en su lugar.
+*/
+
+}
