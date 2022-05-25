06db1c0595a726940e4163c6c9c2f5b8a2bbbfa5
PabloSirCanyada <psirvent78@gmail.com>
2022-05-25 20:10:34 +0200

    Ejercicio 3 Terminado

--- Pablo Sirvent Cañada/Pablo Sirvent Cañada/Deposito.cs
+++ Pablo Sirvent Cañada/Pablo Sirvent Cañada/Deposito.cs
@@ -8,29 +8,34 @@
          * Los métodos permiten reponer o consumir tanto pienso como agua.
          * Estos comentarios se deben eliminar y usar comentarios de documentación en su lugar.
          */
+
         private decimal niveldeagua;
         private decimal niveldepienso;
         private decimal cantidadmaxima;
         private decimal tamañodeposito;
+
         public decimal Niveldeagua
         {
             get { return niveldeagua; }
         }
+
         public decimal Niveldepienso
         {
             get { return niveldepienso; }
         }
+
         public void reponer1(decimal cantidadareponerdeagua)
         {
             decimal res;
+
             tamañodeposito = 10000;
             res = ObtenerNivelSuminstro(cantidadareponerdeagua, tamañodeposito, niveldeagua);
-
         }
 
         private decimal ObtenerNivelSuminstro(decimal cantidadareponer, decimal tamañodeposito, decimal niveldesuministro)
         {
             cantidadmaxima = cantidadareponer + niveldesuministro;
+
             if (cantidadareponer > 0 && cantidadmaxima < tamañodeposito) ;  //Tamaño del depósito de pienso es de 1000 l.
             niveldesuministro = niveldesuministro + cantidadareponer;
 
@@ -40,6 +45,7 @@
         public void reponer2(decimal cantidadareponerdepienso)
         {
             decimal res;
+
             tamañodeposito = 5000;
             res = ObtenerNivelSuminstro(cantidadareponerdepienso, tamañodeposito, Niveldepienso);
         }
@@ -48,6 +54,7 @@
         public decimal consumo1(decimal cantidadaretirardeagua)
         {
             decimal retirado1 = 0; //Cantidad que se retira
+
             if (cantidadaretirardeagua > 0 && cantidadaretirardeagua <= niveldeagua)
             {
                 retirado1 = cantidadaretirardeagua;
@@ -56,9 +63,11 @@
 
             return retirado1;
         }
+
         public decimal consumo2(decimal cantidadaretirardepienso)
         {
             decimal retirado2 = 0; //Cantidad que se retira
+
             if (cantidadaretirardepienso > 0 && cantidadaretirardepienso <= niveldepienso)
             {
                 retirado2 = cantidadaretirardepienso;
@@ -66,13 +75,5 @@
             }
             return retirado2;
         }
-
-
-
-
-
-
-
-
     }
 }
